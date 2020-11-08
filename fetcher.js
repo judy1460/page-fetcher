914bdda3ad8c91fe0ffefc08458a282e37bacdb6
Mina <minatoosa@yahoo.com>
2020-11-08 16:34:15 +0000

    Add print in fetcher

---
+++ fetcher.js
@@ -0,0 +1,14 @@
+const args = process.argv;
+const resource = args[2];
+const path = args[3];
+const fs = require('fs');
+
+const request = require('request');
+request(resource, (error, response, body) => {
+  fs.writeFile( path , body, 'utf8', function(err) {
+    let stats = fs.statSync(path);
+    let fileSize = stats["size"];
+    console.log(`Downloaded and saved ${fileSize} bytes to ${path}`)
+    if (err) reject(err);
+  });
+});
